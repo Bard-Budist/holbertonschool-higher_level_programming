4e4ce156cee19257a93678b7dbfc8d3607647da2
root <op7puro@live.com>
2019-12-13 02:30:03 +0000

    Fix task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-import add_0
-a = 1
-b = 2
 if __name__ == "__main__":
-    print("{:d} + {:d} = {:d}".format(a, b, add_0.add(a, b)))
+    import  add_0 as funcion
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, funcion.add(a, b)))
