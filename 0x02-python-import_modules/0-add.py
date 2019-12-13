38271cb6f37477e56fc3cabbbeb68b130c7d0522
root <op7puro@live.com>
2019-12-13 02:39:14 +0000

    Add identation

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import  add_0 as funcion
+    import add_0 as funcion
     a = 1
     b = 2
     print("{:d} + {:d} = {:d}".format(a, b, funcion.add(a, b)))
