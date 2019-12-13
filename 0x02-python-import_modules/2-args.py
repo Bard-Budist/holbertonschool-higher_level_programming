d2278841436b09632453ef2e92a9b8fffeea64a6
root <op7puro@live.com>
2019-12-13 03:43:04 +0000

    Add task 02

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    i = 0
+    if len(sys.argv) != 1:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
+        for i in range(len(sys.argv)):
+            if i != 0:
+                print("{:d}: {}".format(i, sys.argv[i]))
+    else:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
