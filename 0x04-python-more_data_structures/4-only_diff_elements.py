be0a39e17e0938addd96f17d3e6773c2f63ad97f
GiovanniAyala <op7puro@live.com>
2019-12-19 18:22:47 +0000

    Add task 04

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    new = []
+    new = [x for x in set_1 if x not in set_2]
+    new += [y for y in set_2 if y not in set_1]
+    return new
