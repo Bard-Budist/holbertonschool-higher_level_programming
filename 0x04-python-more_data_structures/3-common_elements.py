fdc868b5a553de2248d20dfe0f6b730a9e528a5c
GiovanniAyala <op7puro@live.com>
2019-12-19 18:09:17 +0000

    Fix task 03

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    for item in set_1:
-        if item in set_2:
-            return item
-    return ''
+    return [x for x in set_1 if x in set_2]
