60914a2cdfa61ba4c112310fe451405cf7caa7ae
GiovanniAyala <op7puro@live.com>
2019-12-19 20:14:08 +0000

    Fix task 10 x6

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,4 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return (sorted(a_dictionary.items(), key = lambda k: (k[1], k[0]))[-1][0])
+    return (sorted(a_dictionary.items(), key = lambda k : (k[1], k[0]))[-1][0])
