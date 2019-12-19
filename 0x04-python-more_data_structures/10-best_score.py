8fbd2b09bbd5fa96707c69da90823900ac96e9d4
GiovanniAyala <op7puro@live.com>
2019-12-19 20:02:22 +0000

    Fix task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not a_dictionary:
-        return None
-    for key in sorted(a_dictionary.keys()):
-        pass
-    return key
+    if a_dictionary == None:
+            return None
+    return (sorted(a_dictionary.items(), key = lambda kg: (kg[1], kg[0]))[-1][0])
