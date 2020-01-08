ec327577061a811dd6c648a834c927a1af463a5d
GiovanniAyala <op7puro@live.com>
2020-01-08 20:35:52 +0000

    Add task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    band = 0
+    mg = "dawd"
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as mg:
+        print("Exception: {}".format(mg), file=sys.stderr)
+        return False
+    return True
