d32a62c4ea94db69b7fcadd85a129c40e4d544f8
GiovanniAyala <op7puro@live.com>
2020-01-08 19:02:02 +0000

    Fix task, Add is not None in condition

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         pass
     finally:
-        if resul != None:
+        if resul is not None:
             print("Inside result: {:.1f}".format(resul))
         else:
             print("Inside result: {}".format(resul))
