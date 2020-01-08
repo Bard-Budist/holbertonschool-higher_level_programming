2379739ee496bdeed4e10af810ab684c8a64a573
GiovanniAyala <op7puro@live.com>
2020-01-08 21:00:31 +0000

    Try fix task

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         resul = fct(args[0], args[1])
-    except (IndexError, ZeroDivisionError, ValueError) as error:
+    except (IndexError, ZeroDivisionError, ValueError, TypeError) as error:
         print("Exception: {}".format(error), file=sys.stderr)
         return None
     return resul
