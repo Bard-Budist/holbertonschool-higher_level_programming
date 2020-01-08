c16c7d154c2134d4c0fb7dc9da1c3fcd56fd972a
GiovanniAyala <op7puro@live.com>
2020-01-08 18:59:41 +0000

    Add task 03

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    resul = None
+    try:
+        resul = a / b
+    except ZeroDivisionError:
+        pass
+    finally:
+        if resul != None:
+            print("Inside result: {:.1f}".format(resul))
+        else:
+            print("Inside result: {}".format(resul))
+    return resul
