c60842784a3937db20af0e23a025633573062ce0
GiovanniAyala <op7puro@live.com>
2020-01-08 18:04:36 +0000

    Tru fix task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,6 +5,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             leng = leng + 1
         except IndexError:
-            pass
+            print("")
+            return leng
     print("")
     return leng
