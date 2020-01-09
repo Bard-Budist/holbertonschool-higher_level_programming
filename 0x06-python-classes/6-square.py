a89684434ea8eff471f100e2ddd72474ee53e18b
GiovanniAyala <op7puro@live.com>
2020-01-09 22:55:35 +0000

    Add task 06

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        docstring for __init__
+        """
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+            self.__position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        """
+            doscstring for area
+        """
+        if (type(size) is not int):
+            raise TypeError("size must be an integer")
+        elif (size < 0):
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def position(self):
+        """
+        Note:
+            Rerturn position
+        """
+        return (self.__position)
+
+    @position.setter
+    def position(self, position):
+        """
+        Note:
+            Set value to Position
+        Args:
+            position (:obj: is a tuple)
+        """
+        if ((type(position) is not tuple)
+                or (positio[0] < 0 or position[1] < 0) or (len(position) < 2)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+    def my_print(self):
+        """docstring for f"""
+        x = self.__size
+        if (x == 0):
+            print()
+        for i in range(x):
+            for item in range(self.position[0]):
+                print("_", end="")
+            for y in range(x):
+                print("#", end="")
+            print()
+
+    def area(self):
+        """docstring for fname"""
+        return (self.__size * self.__size)
