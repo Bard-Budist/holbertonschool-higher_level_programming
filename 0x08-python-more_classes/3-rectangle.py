65979ecf319d1c0908b050aeab93b46787018810
GiovanniAyala <op7puro@live.com>
2020-01-14 01:30:16 -0500

    Add task 00 at 03

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+class Rectangle():
+
+    def __init__(self, width=0, height=0):
+        self.__height = self.checkHeight(height)
+        self.__width = self.checkWidth(width)
+
+    @property
+    def height(self):
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        self.__height = self.checkHeight(value)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = self.checkWidth(value)
+
+    @staticmethod
+    def checkWidth(width):
+        if (type(width) is not int):
+            raise TypeError("width must be an integer")
+        if (width < 0):
+            raise ValueError("width must be >= 0")
+        return width
+
+    @staticmethod
+    def checkHeight(height):
+        if (type(height) is not int):
+            raise TypeError("height must be an integer")
+        if (height < 0):
+            raise ValueError("height must be >= 0")
+        return height
+
+    def area(self):
+        return (self.__height * self.__width)
+
+
+    def perimeter(self):
+        return (2 * (self.__height + self.__width))
+
+    def __str__(self):
+        for x in range(self.__height):
+            for y in range(self.__width):
+                print("#", end="")
+            print()
