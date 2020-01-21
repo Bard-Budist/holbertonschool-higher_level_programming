85d6dc82fdebe989a76090f56061a8902915fadc
GiovanniAyala <op7puro@live.com>
2020-01-21 22:23:12 +0000

    Fix task 09

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 import sys
+import os.path
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 data = []
-if (len(sys.argv) == 1):
+if os.path.isfile("add_item.json"):
     save_to_json_file(data, "add_item.json")
-else:
     data = load_from_json_file("add_item.json")
     for items in sys.argv[1:]:
         data.append(items)
     save_to_json_file(data, "add_item.json")
+else:
+    with open("add_item.json", mode="w+") as file:
+        file.write(str(data))
