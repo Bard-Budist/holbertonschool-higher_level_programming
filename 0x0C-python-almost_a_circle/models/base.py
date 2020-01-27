bb977c39e5117722289a040f48f43eee7f4dc385
GiovanniAyala <op7puro@live.com>
2020-01-27 19:16:33 +0000

    Fix condition

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """
             To_json_String
-        """]
+        """
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return ("[]")
         return (json.dumps(list_dictionaries))
