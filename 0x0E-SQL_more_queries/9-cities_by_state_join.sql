ef792a35f415942960192d16dd778cac7c21b265
root <Bard-Budist>
2020-03-04 01:03:02 +0000

    Fix: Fix task 09

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- Cities By States
 SELECT cities.id, cities.name, states.name FROM cities
-INNER JOIN states ON cities.id = states.id
+INNER JOIN states ON cities.state_id = states.id
 ORDER BY cities.id ASC
