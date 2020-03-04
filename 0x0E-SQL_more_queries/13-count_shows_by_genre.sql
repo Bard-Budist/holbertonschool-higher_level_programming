be294823eeebe656a5198596dd7633fae9e6d7dc
root <Bard-Budist>
2020-03-04 02:30:39 +0000

    Fix: Fix alias un number COUNT

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- Task 13
-SELECT tv_genres.name, COUNT(tv_show_genres.show_id) as number_of_show FROM tv_genres
+SELECT tv_genres.name, COUNT(tv_show_genres.show_id) as number_of_shows FROM tv_genres
 INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
-ORDER BY number_of_show DESC
+ORDER BY number_of_shows DESC
