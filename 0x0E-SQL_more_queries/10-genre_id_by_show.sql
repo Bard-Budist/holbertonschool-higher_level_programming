a54c3458c3f508f2aa543e12dca212e09d6cf746
root <Bard-Budist>
2020-03-04 01:57:51 +0000

    Fix: Fis task 10

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- SHow more
-SELECT tv_show.title, tv_show_genres.genre_id FROM tv_show
-INNER JOIN tv_show_genres ON tv_show.id = tv_show_genres.show_id
-ORDER BY tv_show.title ASC
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
