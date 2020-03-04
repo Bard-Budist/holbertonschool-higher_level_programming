4df09e4ba54416aa3663c3589eff612d17eb1340
root <Bard-Budist>
2020-03-04 04:14:34 +0000

    Fix: Fix task

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,2 @@
 -- Task 13
-SELECT tv_genres.name as genre, COUNT(tv_show_genres.show_id) as number_of_shows FROM tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-GROUP BY genre
-ORDER BY number_of_shows DESC
+SELECT tv_genres.name AS genre, count(*) AS number_of_shows FROM tv_show_genres LEFT JOIN tv_genres ON tv_genres.id = genre_id GROUP BY genre_id ORDER BY count(*) DESC;
