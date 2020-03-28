04ffe0501bbb02968f2dad8e3f4b2e696a05c65a
Bard-Budist <op7puro@live.com>
2020-03-28 12:39:33 -0500

    Fix: Add fix

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -4,11 +4,11 @@ from model_state import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 
+
 class City(Base):
     """Class City"""
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'))
-    
     state_rel = relationship("State", back_populates="cities")
