f65d3f217cda7966ee6025dd3d7943f7774667dd
Bard-Budist <op7puro@live.com>
2020-03-27 20:15:32 -0500

    Feature: Add much task

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import MySQLdb
+import  sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cursor = db.cursor()
+    
+    cursor.execute("SELECT cities.id, cities.name, states.name  FROM cities INNER JOIN states ON states.id = cities.id ORDER BY cities.id ASC")
+    data = cursor.fetchall()
+
+    for row in data:
+        print (row)
---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import MySQLdb
+import  sys
+import re
+
+if __name__ == "__main__":
+    result = []
+    db = MySQLdb.connect(host='localhost', user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cursor = db.cursor()
+
+    cursor.execute("SELECT cities.name FROM cities INNER JOIN states ON states.id = cities.id WHERE states.name = %s ORDER BY cities.id ASC", [sys.argv[4]])
+    data = cursor.fetchall()
+
+    for row in data:
+        result.append(re.search("'(.*)'", (str)(row)).group(1))
+
+    for x in range(len(result)):
+        print(result[x], end='')
+        if x != len(result) - 1:
+            print(", ", end='')
+    print()
