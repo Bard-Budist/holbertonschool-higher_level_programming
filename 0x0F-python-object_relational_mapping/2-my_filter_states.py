86bb5f87d5db3f3acceed20ffa33982cd9e35863
Bard-Budist <op7puro@live.com>
2020-03-28 19:36:07 +0000

    try Fix

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
-            (sys.argv[4],))
+    cursor.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", (sys.argv[4],))
     data = cursor.fetchall()
 
     for row in data:
