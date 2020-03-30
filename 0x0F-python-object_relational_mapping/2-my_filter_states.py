5ec3a7b13a00941a1a4b65e672d30bef795557c1
Bard-Budist <op7puro@live.com>
2020-03-29 20:37:25 -0500

    Fix fk error

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -9,11 +9,11 @@ if __name__ == "__main__":
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name = (%s)\
+                    ORDER BY id ASC", [sys.argv[4]])
     data = cursor.fetchall()
 
     for row in data:
-        if row[1] == sys.argv[4]:
-            print(row)
+        print(row)
     cursor.close()
     db.close()
