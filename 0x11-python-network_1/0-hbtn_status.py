36fe7197e5b42b39a4fcf9db69c3dddba5a47a81
Bard-Budist <op7puro@live.com>
2020-04-15 12:36:06 -0500

    Fix: add tawsk

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,8 +5,5 @@ import urllib.request
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as rp:
         body = rp.read()
-        print(
-            """Body response:
-            \t- type: {}
-            \t- content: {}
-            \t- utf8 content: {}""".format(type(body), body, "OK"))
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\
+        - utf8 content: {}".format(type(body), body, "OK"))
