9043d5dea03e10b726bb73e513203c918bfb035e
Bard-Budist <op7puro@live.com>
2020-04-16 10:36:22 -0500

    Fix: fix task

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,5 +6,5 @@ import sys
 if __name__ == "__main__":
     data = {'Authorization': 'token ' + sys.argv[2]}
     user = sys.argv[1]
-    rp = requests.get('https://api.github.com/user',auth=(user, sys.argv[2]))
+    rp = requests.get('https://api.github.com/user', auth=(user, sys.argv[2]))
     print(rp.json().get('id'))
