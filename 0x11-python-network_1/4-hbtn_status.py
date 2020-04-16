59d4fd933ccd7ef120d91777f3561628800eb392
Bard-Budist <op7puro@live.com>
2020-04-16 09:40:09 -0500

    Fix: Fix full task

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@ import requests
 
 
 if __name__ == "__main__":
-    with requests.get('https://intranet.hbtn.io/status') as rp:
-        print("Body response:")
-        print("\t- type: {}".format(type(rp.content.decode('utf-8'))))
-        print("\t- content: {}".format(rp.content.decode('utf-8')))
+    rp = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(rp.content.decode('utf-8'))))
+    print("\t- content: {}".format(rp.content.decode('utf-8')))
