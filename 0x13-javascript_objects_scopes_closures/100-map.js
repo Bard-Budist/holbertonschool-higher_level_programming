7e1b90794e8e1b273b97c297dcbba48806917986
Bard-Budist <op7puro@live.com>
2020-05-11 15:07:05 -0500

    Add task

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const new_list = [];
-for (const y in list) {
-  new_list[y] = list[y] * y;
-}
+let count = 0;
+const newList = list.map(x => x * count++);
 console.log(list);
-console.log(new_list);
+console.log(newList);
