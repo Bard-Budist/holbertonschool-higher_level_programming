0e9c94cc8856274c7d4112f087a73853536bc349
Bard-Budist <op7puro@live.com>
2020-05-11 15:01:30 -0500

    Add execute file

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 const new_list = [];
-for (const i in list) {
-  new_list[i] = list[i] * i;
+for (const y in list) {
+  new_list[y] = list[y] * y;
 }
 console.log(list);
 console.log(new_list);
