640cbc3329d208588b02d3ce9ae95e53b0cafee4
Bard-Budist <op7puro@live.com>
2020-05-11 15:11:57 -0500

    Add dat

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 let count = 0;
-const newList = list.map(x => x * count++);
+const newList = list.map(i => i * count++);
 console.log(list);
 console.log(newList);
